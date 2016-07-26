5484a4adaeffa7fbef84374a7fc289574e4da512
Shane Thompson <shane.thompson@aeontech.com.au>
2016-07-26 14:37:42 +0800

    Moved WPide.php to AceIDE.php
    Starting to rename the project to AceIDE. WPide.php has
    been moved to AceIDE.php and the file annotations have been
    changed to reflect the new project.

--- AceIDE.php
+++ AceIDE.php
@@ -1,12 +1,11 @@
 <?php
-/*
-Plugin Name: WPide
-Plugin URI: https://github.com/WPsites/WPide
-Description: WordPress code editor with auto completion of both WordPress and PHP functions with reference, syntax highlighting, line numbers, tabbed editing, automatic backup.
-Version: 2.4.0
-Author: Simon @ WPsites
-Author URI: http://www.wpsites.co.uk
-*/
+/**
+ * Plugin Name: AceIDE
+ * Plugin URI: https://github.com/AceIDE/AceIDE
+ * Description: WordPress code editor with auto completion of both WordPress and PHP functions with reference, syntax highlighting, line numbers, tabbed editing, automatic backup.
+ * Version: 2.5.0
+ * Author: AceIDE
+ **/
 
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
