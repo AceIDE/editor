5dedd296ffcc7ee9d17211f74e61d4a18731ea0c
Shane Thompson <shanept@iinet.net.au>
2016-07-26 23:40:22 +0800

    Added error when vendor directory is missing
    Upon activation of the AceIDE plugin, it will now error
    if the vendor directory is missing.

--- AceIDE.php
+++ AceIDE.php
@@ -10,6 +10,10 @@
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
+if ( !file_exists( __DIR__ . '/vendor/' ) ) {
+	trigger_error( 'Composer "vendor/" directory missing.', E_USER_ERROR );
+}
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $aceide = new AceIDE\Editor\AceIDE;
