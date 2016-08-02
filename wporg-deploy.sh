11ddcd4c4edbabbccde711192c7df887992bc189
Shane Thompson <shane.thompson@aeontech.com.au>
2016-08-02 11:15:40 +0800

    Modified deploy script for WordPress.

--- wporg-deploy.sh
+++ wporg-deploy.sh
@@ -9,17 +9,17 @@
 # This script needs to be executable (chmod +x wporg-deploy.sh)
 
 # main config
-PLUGINSLUG="wpide"
+PLUGINSLUG="aceide"
 CURRENTDIR=`pwd`
-MAINFILE="WPide.php" # this should be the name of your main php file in the wordpress plugin
+MAINFILE="AceIDE.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
 GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/wpide/" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="WPsites" # your svn username
+SVNURL="http://plugins.svn.wordpress.org/aceide/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNUSER="shanept" # your svn username
 SVNIGNORE="
 backups
 .DS_Store" #other files you you don't need to publish to the WP.org repo
