7588bcd973e579f5c47bc3c842c29c05c0b26dd7
Shane Thompson <me@shane.pt>
2018-01-28 10:49:38 +0800

    (#27) Fixes AJAX issue

--- src/Ajax.php
+++ src/Ajax.php
@@ -5,6 +5,10 @@
 		ob_start();
 	}
 
+	// WordPress will automatically assume we are in the wp-admin directory.
+	// Defining the following will let WP know this is not the case.
+	define( 'WP_ADMIN', false );
+
 	// Now, we include admin-ajax.php as if nothing happened...
 	require dirname(						// WP base install dir
 		dirname(							// wp-content
